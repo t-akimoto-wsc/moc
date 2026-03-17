4dfa85919afb3a66ba4b5e034cf8697c0614dade
y-gotou-wsc <y-gotou@worth-sc.jp>
2026-03-17 11:17:11 +0900

    Update si003.dart

--- lib/si003.dart
+++ lib/si003.dart
@@ -84,7 +84,7 @@ class _PasswordResetState extends State<PasswordReset> {
     setState(() => message = null);
 
     Navigator.of(context).pushReplacement(
-      MaterialPageRoute(builder: (_) => const screen005.OtpScreen(from: OtpFrom.signup)),
+      MaterialPageRoute(builder: (_) => const screen005.OtpScreen(from: OtpFrom.resetPassword)),
     );
   }
 
